7f40f9f264ebb7cdc6baf04ef733677c2f98fd29
Sam Sartor <me@samsartor.com>
2017-11-26 22:27:36 -0700

    physics guru

--- src/common/gurus/physics.rs
+++ src/common/gurus/physics.rs
@@ -1,3 +1,36 @@
+use nphysics3d::world::World;
+use nphysics3d::object::RigidBody;
+use nalgebra::{Vector3, Isometry3};
+use std::rc::Rc;
+use std::cell::RefCell;
+
 pub struct PhysicsGuru {
-    
+    world: World<f32>,
+}
+
+impl PhysicsGuru {
+    pub fn new(gravity: Vector3<f32>) -> PhysicsGuru {
+        let mut world = World::new();
+        world.set_gravity(gravity);
+        PhysicsGuru {
+            world: world,
+        }
+    }
+
+    pub fn body(&mut self, body: RigidBody<f32>)
+        -> impl FnOnce(&PhysicsReply)
+        -> RigidBody<f32> 
+    {
+        let body = self.world.add_rigid_body(body);
+        move |_| body.try_unwrap().expect("Guru not resolved")
+    }
+
+    pub fn resolve(self, dt: f32) -> PhysicsReply {
+        self.world.step(dt);
+        PhysicsReply { _me: (), }
+    }
+}
+
+pub struct PhysicsReply {
+    _me: (),
 }
