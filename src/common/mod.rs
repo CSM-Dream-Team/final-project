34fc5a15fe613492029bfc19e525f8ea289044b3
Sumner Evans <sumner.evans98@gmail.com>
2017-11-26 20:45:41 -0700

    Starting on Common

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,14 +1,31 @@
-use gfx;
-use flight::draw::{Painter, PbrStyle, SolidStyle, UnishadeStyle};
-
 pub mod gurus;
 
-pub struct Meshes {
+use gfx;
 
+use flight::PbrMesh;
+use flight::draw::{Painter, PbrStyle, SolidStyle, UnishadeStyle};
+use flight::mesh::*;
+
+use self::gurus::*;
+
+pub struct Meshes {
+    controller: PbrMesh<R>,
+    grid_lines: Mesh<R, VertC, ()>,
 }
 
 pub struct Painters<R: gfx::Resources> {
     solid: Painter<R, SolidStyle<R>>,
     pbr: Painter<R, PbrStyle<R>>,
     unishade: Painter<R, UnishadeStyle<R>>,
+}
+
+pub struct Gurus {
+    interact: interact::InteractGuru,
+    physics: physics::PhysicsGuru,
+}
+
+pub struct Common<R: gfx::Resources> {
+    gurus: Gurus,
+    painters: Painters<R>,
+    meshes: Meshes,
 }
