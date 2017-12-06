61623f76f9d78c651cea739366eec0f9203c8c45
Sam Sartor <me@samsartor.com>
2017-12-06 01:57:49 -0700

    physics change

--- src/app/lets_get_physical.rs
+++ src/app/lets_get_physical.rs
@@ -1,6 +1,6 @@
 use std::boxed::FnBox;
 
-use nalgebra::{self as na, Vector3, Isometry3};
+use nalgebra::{self as na, Vector3, Isometry3, Translation3, Similarity3, UnitQuaternion};
 use ncollide::shape::{ShapeHandle, Compound, Cuboid, Cylinder};
 use nphysics3d::object::RigidBody;
 
@@ -22,11 +22,18 @@ pub struct LetsGetPhysical<R: gfx::Resources> {
 impl<R: gfx::Resources> LetsGetPhysical<R> {
     pub fn new<F: gfx::Factory<R>>(factory: &mut F) -> Result<Self, Error> {
         let shapes = vec! [
-            (Isometry3::new(Vector3::new(0.,-3.,0.) * 0.08, na::zero()) , ShapeHandle::new(Cuboid::new(Vector3::new(2.0, 1.5 , 1.5) * 0.08))),
-            (Isometry3::new(Vector3::new(0.,1.25,0.) * 0.08, na::zero()) , ShapeHandle::new(Cylinder::new(3.25 * 0.08, 0.5 * 0.08))),
+            (
+                Isometry3::new(Vector3::new(0.,-3.,0.) * 0.08, na::zero()),
+                ShapeHandle::new(Cuboid::new(Vector3::new(2.0, 1.5 , 1.5) * 0.08))
+            ),
+            (
+                Isometry3::new(Vector3::new(0.,1.25,0.) * 0.08, na::zero()),
+                ShapeHandle::new(Cylinder::new(3.25 * 0.08, 0.5 * 0.08))
+            ),
         ];
         let compound = Compound::new(shapes);
-        let mjolnir_body = RigidBody::new_dynamic(compound, 2330., 0.35, 0.47);
+        let mut mjolnir_body = RigidBody::new_dynamic(compound, 2330., 0.35, 0.47);
+        mjolnir_body.set_transformation(Isometry3::new(Vector3::new(0., 0.5, 0.), na::zero()));
 
         Ok(LetsGetPhysical {
             mjolnir: load::object_directory(factory, "assets/hammer/")?,
